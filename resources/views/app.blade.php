f8b27f0b1cbc7b4b313c3f990cb19fa8c726bf03
tymur778 <8149705+tymur778@users.noreply.github.com>
2023-03-24 17:49:41 +0100

    Deploy test

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="robots" content="noindex">
+        <!-- test -->
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
